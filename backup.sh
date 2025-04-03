8afb9d531250031a30f7535e8c50291dcf17ccf9
creed121 <148902946+creed121@users.noreply.github.com>
2025-04-03 11:32:55 -0400

    Add /var backups to backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # backup.sh
-# Author: Christopher Reed
 # Performs both local and remote backups
 
 # Verifies sudo was used
@@ -16,8 +15,12 @@ if [ -n ${BACKUP_USER} ] && [ -n ${TEAM_NUM} ]
 then
 	# Declare Variables
 	BACKUP_SERVER="${BACKUP_USER}@192.168.${TEAM_NUM}.15:~/backups"
+	VAR_DIR="/backups/var"
+	LIB_DIR="/backups/lib"
 	ETC_FILE="/backups/etc_backup.txt"
 	HOME_FILE="/backups/home_backup.txt"
+	VAR_FILE="/backups/var_backup.txt"
+	LIB_FILE="/backups/lib_backup.txt"
 	REMOTE_FILE="/backups/curr_backup.txt"
 	SSH_KEY="/root/backup_key"
 	
@@ -26,14 +29,20 @@ then
 	# > /dev/null removes command line output
 	
 	# Local Backups
-	rsync -abv --delete /etc/ /backups/etc --log-file=${ETC_FILE} > /dev/null
+	rsync -abv --delete /etc /backups --log-file=${ETC_FILE} > /dev/null
 	printf "\n\nSEARCHABLE TEXT\n\n" >> ${ETC_FILE}
-	rsync -abv --delete /home/ /backups/home --log-file=${HOME_FILE} > /dev/null
+	rsync -abv --delete /home /backups --log-file=${HOME_FILE} > /dev/null
 	printf "\n\nSEARCHABLE TEXT\n\n" >> ${HOME_FILE}
+	rsync -abv --delete /var/auth ${VAR_DIR} --log-file=${VAR_FILE} > /dev/null
+	rsync -abv --delete /var/log ${VAR_DIR} --log-file=${VAR_FILE} > /dev/null
+	rsync -abv --delete /var/spool ${VAR_DIR} --log-file=${VAR_FILE} > /dev/null
+	printf "\n\nSEARCHABLE TEXT\n\n" >> ${VAR_FILE}
+	rsync -abv --delete /lib/systemd ${LIB_DIR} --log-file=${LIB_FILE} > /dev/null
+	printf "\n\nSEARCHABLE TEXT\n\n" >> ${LIB_FILE}
 	
 	# Remote Backup
 	# Remove # after first run (required to establish connection)
-	rsync -abv --delete /backups/ ${BACKUP_SERVER}/${HOSTNAME} --log-file=${REMOTE_FILE} -e "ssh -i ${SSH_KEY}" #> /dev/null
+	rsync -abv --delete /backups ${BACKUP_SERVER}/${HOSTNAME} --log-file=${REMOTE_FILE} -e "ssh -i ${SSH_KEY}" #> /dev/null
 	printf "\n\nSEARCHABLE TEXT\n\n" >> ${REMOTE_FILE}
 	scp -i ${SSH_KEY} ${REMOTE_FILE} ${BACKUP_SERVER}/curr_${HOSTNAME}_backup.txt
 else
