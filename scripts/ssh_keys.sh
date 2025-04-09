5207d8889a26a1a7b77d10be0bfb67dfeff858b7
creed121 <148902946+creed121@users.noreply.github.com>
2025-04-09 00:50:08 -0400

    Create ssh_keys.sh

---
+++ scripts/ssh_keys.sh
@@ -0,0 +1,27 @@
+add_key () {
+	KEY=$1
+	USER=$2
+	SSH_DIR="/home/${USER}/.ssh"
+	AUTHORIZED_KEYS="${SSH_DIR}/authorized_keys"
+	if [ ! -d ${SSH_DIR} ]; then
+		mkdir -p ${SSH_DIR}
+		chmod 700 ${SSH_DIR}
+		chown "${USER}:${USER}" 
+	fi
+	cp ${KEY} ${SSH_DIR}
+	chmod 600 ${SSH_DIR}
+	chown "${USER}:${USER}" ${AUTHORIZED_KEYS}
+}
+
+# Add scoring SSH key to each scoring user
+for user in $(./dynamic_files/scoring_users.txt); do
+    add_key ./dynamic_files/scoring_key.pub $user
+done
+# Add SSH key for David to do hardening
+add_key ./dynamic_files/david_key.pub blueteam
+
+# Enable and restart services
+systemctl enable ssh
+systemctl enable sshd
+systemctl restart ssh
+systemctl restart sshd
