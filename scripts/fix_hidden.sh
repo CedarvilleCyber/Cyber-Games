64e167710465b509ecd61a472b7fd513bbb0c2ff
creed121 <148902946+creed121@users.noreply.github.com>
2025-04-12 15:15:09 -0400

    Update fix_hidden.sh

--- scripts/fix_hidden.sh
+++ scripts/fix_hidden.sh
@@ -1,6 +1,6 @@
 ```Shell
-for user in $(./dynamic_files/scoring_users.txt); do
+mv /home/.blueteam /home/blueteam
+for user in $(/home/blueteam/Cyber-Games/dynamic_files/scoring_users.txt); do
   mv /home/.$user /home/$user
 done
-mv /home/.blueteam /home/blueteam
 ```
